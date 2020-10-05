b8b72b25da0ee339d835d795c1ec4c08b7c5ecab
Saif Elnewehy <saif.elnewehy@aiesec.net.eg>
2020-10-05 16:22:16 +0200

    Apps Creation

--- crm_tool/settings.py
+++ crm_tool/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'general',
+    'ams',
+    'applications',
+    'tm',
+    'sign_ups',
+    'opportunities',
 ]
 
 MIDDLEWARE = [
